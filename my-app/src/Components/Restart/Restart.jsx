1edf43a54e2beb26cc90937e3f04077d902b0b15
Mulc1ber <dmo@mail.ru>
2024-10-15 22:26:02 +0300

    change connect dispatch

--- my-app/src/Components/Restart/Restart.jsx
+++ my-app/src/Components/Restart/Restart.jsx
@@ -1,14 +1,7 @@
 import { connect } from 'react-redux';
 import { setIsCurrentPlayer, setFields, setIsDraw, setIsGameEnded } from '../../Actions';
 
-export const RestartContainer = ({ dispatch, isGameEnded }) => {
-    const handleRestart = () => {
-        dispatch(setFields(Array(9).fill('')));
-        dispatch(setIsCurrentPlayer('X'));
-        dispatch(setIsGameEnded(false));
-        dispatch(setIsDraw(false));
-    };
-
+export const RestartContainer = ({ handleRestart, isGameEnded }) => {
     return (
         <button
             className="px-4 py-2 rounded-lg border-0 bg-black text-white text-base disabled:opacity-50"
@@ -24,4 +17,13 @@ const mapStateToProps = (state) => ({
     isGameEnded: state.isGameEnded,
 });
 
-export const Restart = connect(mapStateToProps)(RestartContainer);
+const mapDispatchToProps = (dispatch) => ({
+    handleRestart: () => {
+        dispatch(setFields(Array(9).fill('')));
+        dispatch(setIsCurrentPlayer('X'));
+        dispatch(setIsGameEnded(false));
+        dispatch(setIsDraw(false));
+    },
+});
+
+export const Restart = connect(mapStateToProps, mapDispatchToProps)(RestartContainer);
